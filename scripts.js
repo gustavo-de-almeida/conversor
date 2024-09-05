e14ed3c3fec2c2c6f737228ef7af180966cd85d5
gustavofdealmeida <gusfranca31@gmail.com>
2024-09-05 16:48:04 -0300

    muitas modificacoes

--- scripts.js
+++ scripts.js
@@ -1,13 +1,18 @@
 const botaoconverter = document.querySelector("button");
 const selecioneamoeda = document.querySelector("#escolhe-moedas");
-function coverter() {
+
+async function coverter() {
   const inputvalor = document.querySelector(".inputmoedas").value;
   const praconverte = document.querySelector(".valor-pra-converter");
   const convertida = document.querySelector(".valor-convertido");
-  const eurohoje = 5.63;
-  const dolarhoje = 5.2;
-const bithoje = 355.90044
-const librahoje = 6.68
+  const moedasapi = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL,GBP-BRL,JPY-BRL").then((response) => response.json());
+
+  const eurohoje = moedasapi.USDBRL.high;
+  const dolarhoje = moedasapi.EURBRL.high;
+  const bithoje = moedasapi.BTCBRL.high;
+  const librahoje = moedasapi.GBPBRL.high;
+  const ienehoje = moedasapi.JPYBRL.high;
+  
 
   if (selecioneamoeda.value == "dolar") {
     convertida.innerHTML = new Intl.NumberFormat("en-US", {
@@ -20,62 +25,65 @@ const librahoje = 6.68
       style: "currency",
       currency: "EUR",
     }).format(inputvalor / eurohoje);
- 
-}
+  }
 
-if (selecioneamoeda.value == "btc"){
-convertida.innerHTML = new Intl.NumberFormat("BTC", {
-  style: 'currency', currency: 'BTC', minimumFractionDigits: 4
-}).format(inputvalor/ bithoje);}
-
-if (selecioneamoeda.value == "libra"){
-  convertida.innerHTML = new Intl.NumberFormat("en-gb", {
-    style: "currency",
-    currency: "GBP",
-  }).format(inputvalor/ librahoje);}
+  if (selecioneamoeda.value == "btc") {
+    convertida.innerHTML = new Intl.NumberFormat("BTC", {
+      style: "currency",
+      currency: "BTC",
+      minimumFractionDigits: 4,
+    }).format(inputvalor / bithoje);
+  }
+  if (selecioneamoeda.value == "iene"){
+    convertida.innerHTML = new Intl.NumberFormat('ja-JP', { 
+      style: 'currency',
+      currency: 'JPY' })
+      .format(inputvalor / ienehoje)
   
+  }
 
-
-
+  if (selecioneamoeda.value == "libra") {
+    convertida.innerHTML = new Intl.NumberFormat("en-gb", {
+      style: "currency",
+      currency: "GBP",
+    }).format(inputvalor / librahoje);
+  }
+  
 
   praconverte.innerHTML = new Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL",
   }).format(inputvalor);
-
-
-
 }
 
-function c (){
-    
-    const nomemoeda = document.querySelector("#dollar")
-        const logos = document.querySelector(".logodamoeda")
-    if(selecioneamoeda.value== "dolar"){
-        nomemoeda.innerHTML = "Dolár americano"
-        logos.src = "./assets/estados-unidos (1) 1.png"
-    }
-    if(selecioneamoeda.value== "Euro"){
-        nomemoeda.innerHTML = "Euro"
-        logos.src = "./assets/Design sem nome 3.png"
-    }
-
-    if(selecioneamoeda.value== "btc"){
-      nomemoeda.innerHTML = "Bitcoin"
-      logos.src = "./assets/bitcoin 1.png"
+function c() {
+  const nomemoeda = document.querySelector("#dollar");
+  const logos = document.querySelector(".logodamoeda");
+  if (selecioneamoeda.value == "dolar") {
+    nomemoeda.innerHTML = "Dolár americano";
+    logos.src = "./assets/estados-unidos (1) 1.png";
+  }
+  if (selecioneamoeda.value == "Euro") {
+    nomemoeda.innerHTML = "Euro";
+    logos.src = "./assets/Design sem nome 3.png";
   }
 
-  if(selecioneamoeda.value== "libra"){
-    nomemoeda.innerHTML = "Libra"
-    logos.src = "./assets/libra 1.png"
+  if (selecioneamoeda.value == "btc") {
+    nomemoeda.innerHTML = "Bitcoin";
+    logos.src = "./assets/bitcoin 1.png";
+  }
+
+  if (selecioneamoeda.value == "libra") {
+    nomemoeda.innerHTML = "Libra";
+    logos.src = "./assets/libra 1.png";
+  }
+  if (selecioneamoeda.value == "iene") {
+    nomemoeda.innerHTML = "Iene";
+    logos.src = "./assets/Iene.png";
+  }
+
+  coverter();
 }
 
-
-
-    coverter()
-}
-
-
-
-selecioneamoeda.addEventListener("change", c)
+selecioneamoeda.addEventListener("change", c);
 botaoconverter.addEventListener("click", coverter);
